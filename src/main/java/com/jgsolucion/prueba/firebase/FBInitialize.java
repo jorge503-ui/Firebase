a95a0bf4f840c435baa4e27df58bc78be5eaa604
Jorgep503 <jorge.p.391@gmail.com>
2022-03-22 21:49:57 -0600

    Cambio de conexion de Firebase a variable de entorno

--- src/main/java/com/jgsolucion/prueba/firebase/FBInitialize.java
+++ src/main/java/com/jgsolucion/prueba/firebase/FBInitialize.java
@@ -3,34 +3,18 @@ package com.jgsolucion.prueba.firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
-import java.nio.file.Paths;
-import java.nio.file.Path;
 
 @Service
 public class FBInitialize {
 
-    //@Value("classpath:Firebase/reslyapp-firebase-adminsdk-xem7v-762e48ee72.json")
-    //private Resource res;
-
-    @Autowired
-    private Environment env;
 
     @PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccount =
-                    new FileInputStream(env.getProperty("GOOGLE_APPLICATION_CREDENTIALS"));
-
             FirebaseOptions options = FirebaseOptions.builder()
-                    //.setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .setCredentials(GoogleCredentials.getApplicationDefault())
                     .setDatabaseUrl("https://reslyapp.firebaseio.com/")
                     .build();
